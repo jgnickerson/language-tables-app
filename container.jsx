8e6b37324fbd170ab59459d5e23494ea0da8e4fa
jgnickerson <jgnickerson@me.com>
2016-10-02 14:53:08 -0400

    initial setup done

---
+++ container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Container extends React.Component {
+  render() {
+    return (<h1>Language Tables Scheduler</h1>)
+  }
+}
+
+ReactDOM.render(<Container/>, document.getElementById('container'));
