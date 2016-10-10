32c409b1090113d6737bb747cb002eeaffe358a5
jgnickerson <jgnickerson@me.com>
2016-10-09 20:14:15 -0400

    update to reactv15

--- container.jsx
+++ container.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class Container extends React.Component {
   render() {
-    return (<h1>Language Tables Scheduler</h1>)
+    return <h1>Language Tables Signup</h1>
   }
 }
 
