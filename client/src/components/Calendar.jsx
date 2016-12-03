adcf4ad42af35baa37ec76302281e2f70a84997c
jgnickerson <jgnickerson@me.com>
2016-12-03 11:21:17 -0500

    calendar coloring working

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import _ from 'lodash'
 import $ from 'jquery'
 import moment from 'moment'
+import FullCalendar from 'rc-calendar/lib/FullCalendar'
 import RCalendar from 'rc-calendar'
-import styles from 'rc-calendar/assets/index.css'
+import Select from 'rc-select'
+import 'rc-select/assets/index.css'
+import 'rc-calendar/assets/index.css'
+import '../styles/main.css'
+
+
 
 //change the date availability to be a prop that gets passed down from signup
 var Calendar = React.createClass({
@@ -26,14 +32,49 @@ var Calendar = React.createClass({
     })
   },
 
+  // <RCalendar
+  //   showToday={false}
+  //   showDateInput={false}
+  //   onSelect={this.checkDate}
+  // />
+  content: function(date) {
+    let content;
+    let availability = this.checkDate(date);
+
+    console.log(availability && availability.seats === 0);
+    if (availability && availability.seats === 0) {
+      content = (
+        <div className={"date-cell waitlist"}>
+        {date.format("D")}
+        </div>
+      );
+    } else if (availability) {
+      content = (
+        <div className={'date-cell available'}>
+        {date.format("D")}
+        </div>
+      );
+    } else {
+      content = (
+        <div className={'date-cell unavailable'}>
+        {date.format("D")}
+        </div>
+      );
+    }
+
+    return content;
+  },
+
   render : function() {
     return (
       <div>
         <h2>Date</h2>
-        <RCalendar
-          showToday={false}
-          showDateInput={false}
-          onSelect={this.checkDate}
+        <FullCalendar
+          style={{ margin:10 }}
+          Select={Select}
+          fullscreen={false}
+          //onSelect={this.checkDate}
+          dateCellRender={this.content}
         />
       </div>
     );
@@ -44,15 +85,18 @@ var Calendar = React.createClass({
       return moment(date, "MM-DD-YYYY").isSame(event.date, 'day');
     });
 
-    let message = "There is no availability on " + date.format("MM-DD");
-    if (availability) {
-       message = "There are " + availability.seats + " seats available on " + availability.date.format("MM-DD");
-    }
-    window.alert(message);
+    return availability;
 
-    if (availability) {
-      this.props.onChange(availability.date);
-    }
+    // let message = "There is no availability on " + date.format("MM-DD");
+    // if (availability) {
+    //    message = "There are " + availability.seats + " seats available on " + availability.date.format("MM-DD");
+    // }
+    // window.alert(message);
+
+    // if (availability) {
+    //   this.props.onChange(availability.date);
+    // }
+
   }
 });
 
