a389f4f2407c6cced479cb4974ef5cf5653525dd
jgnickerson <jgnickerson@me.com>
2016-12-03 17:29:42 -0500

    add basic routing

---
+++ client/src/components/Admin.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Admin = React.createClass({
+  render : function() {
+    return (
+      <div>
+        <h2>You've made a reservation for Language Tables on</h2>
+      </div>
+    );
+  }
+});
+
+module.exports = Admin;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Signup from './components/Signup'
+import Signup from './components/Signup';
+import Admin from './components/Admin';
 import styles from './styles/main';
 import 'react-select/dist/react-select.css';
 
-class App extends React.Component {
-  render() {
+const App = React.createClass({
+  getInitialState: function() {
+    return {
+      route: window.location.hash.substr(1)
+    }
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  },
+
+  render: function() {
+    let Child;
+    console.log(this.state.route);
+    switch (this.state.route) {
+        case 'admin': Child = Admin; break;
+        default: Child = Signup;
+    }
     return (
         <div className="container">
-          <Signup />
+          <Child />
         </div>
     );
   }
-}
+});
 
 ReactDOM.render(
   <App />,
