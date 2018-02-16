7f56697009278c16d8bbb231fbc5d9e1db98848a
Dylan Quenneville <dquenneville@middlebury.edu>
2018-02-15 23:46:28 -0500

    add Cantonese, alphabetize languages

--- constants.js
+++ constants.js
@@ -1,17 +1,21 @@
+// these are in alphabetical order rather than order of language ID because
+// this is directly the order that will be used for the language selection
+// menu on the webpage
 const languages = {
   ARABIC: 0,
   ASL: 1,
+  CANTONESE: 13,
   CHINESE: 2,
   FRENCH: 3,
   GERMAN: 4,
   HEBREW: 5,
   ITALIAN: 6,
+  KOREAN: 12,
   JAPANESE: 7,
   PORTUGUESE: 8,
   RUSSIAN: 9,
   SPANISH: 10,
   SWAHILI: 11,
-  KOREAN: 12
 }
 
 module.exports = { languages: languages };
